7df4f3c6b5820b4d81c09457fdc4534dde0a334f
MrYuriyS <yura_salimov@meta.ua>
2017-01-05 22:34:55 +0200

    Code refactoring.

--- src/main/java/com/salimov/yurii/Main.java
+++ src/main/java/com/salimov/yurii/Main.java
@@ -1,35 +1,25 @@
 package com.salimov.yurii;
 
-import java.util.InputMismatchException;
-import java.util.Scanner;
-
-public class Main {
+public final class Main {
 
     public static void main(String[] args) {
         System.out.println("--- Train Length ---");
-        final Train train = new Train(inputLength());
-        final TrainLength trainLength = new TrainLength(train);
-        System.out.println("Result:");
-        System.out.println("Real length = " + train.getLength());
-        System.out.println("Calculated length = " + trainLength.getLength());
+        final Train train = new Train(
+                ScanLength.scan()
+        );
+        final CalculateLength calculateLength = new CalculateLength(train);
+        printResult(
+                train.getLength(),
+                calculateLength.get()
+        );
     }
 
-    private static int inputLength() {
-        final Scanner scanner = new Scanner(System.in);
-        int length;
-        while (true) {
-            System.out.print("Input train length = ");
-            length = scanner.nextInt();
-            if (length > 0) {
-                break;
-            } else {
-                System.out.println(
-                        "Incorrect input value! " +
-                                "Value must be greater than zero. " +
-                                "Try again..."
-                );
-            }
-        }
-        return length;
+    private static void printResult(
+            final int realLength,
+            final int calculatedLength
+    ) {
+        System.out.println("Result:");
+        System.out.println("Real length = " + realLength);
+        System.out.println("Calculated length = " + calculatedLength);
     }
 }
