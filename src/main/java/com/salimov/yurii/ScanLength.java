cc399344b6949836e14151bc3c21cbe3b3eae78c
MrYuriyS <yura_salimov@meta.ua>
2017-01-06 10:37:07 +0200

    Implemented interface IScanLength and refactored code.

--- src/main/java/com/salimov/yurii/ScanLength.java
+++ src/main/java/com/salimov/yurii/ScanLength.java
@@ -1,27 +1,42 @@
 package com.salimov.yurii;
 
+import java.io.InputStream;
 import java.util.Scanner;
 
 /**
  * The class describes a method for scanning
- * the length of a train with a console.
+ * the length of a train with a input stream.
  * The value is entered by the user.
  *
  * @author Yurii Salimov (yurii.alex.salimov@gmail.com)
  * @version 1.0
  */
-public final class ScanLength {
+public final class ScanLength implements IScanLength {
 
     /**
-     * Scans the length of a train with a console.
+     * The input stream to be scanned
+     */
+    private final InputStream stream;
+
+    /**
+     * Constructor.
+     *
+     * @param stream An input stream to be scanned.
+     */
+    public ScanLength(final InputStream stream) {
+        this.stream = stream;
+    }
+
+    /**
+     * Scans the length of a train with a input stream.
      * The user enters a value as long as the length
      * is greater than zero.
      *
      * @return The train length (number of wagons).
      */
-    public static int scan() {
+    public int scan() {
         int length;
-        try (final Scanner scanner = new Scanner(System.in)) {
+        try (final Scanner scanner = new Scanner(this.stream)) {
             while (true) {
                 System.out.print("Input train length = ");
                 length = scanner.nextInt();
